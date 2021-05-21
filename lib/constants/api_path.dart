2050d0519b0abff5ea44d5f9b53591d7e66cf633
Mohamed Kabbani <kabbani13666@gmail.com>
2021-05-21 17:59:50 +0300

    adding connection to the hub

---
+++ lib/constants/api_path.dart
@@ -0,0 +1,3 @@
+class ApiPath {
+  static const String DomainURI = 'http://kabanisenior-001-site1.gtempurl.com';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/io_client.dart';
+import 'package:signalr_core/signalr_core.dart';
+import 'package:university_transportation_driver/constants/api_path.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,30 +14,97 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'University Transportation Driver',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: UniversityTransportationDriver(
+          title: 'University Transportation Driver'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class UniversityTransportationDriver extends StatefulWidget {
+  UniversityTransportationDriver({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _UniversityTransportationDriverState createState() =>
+      _UniversityTransportationDriverState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _UniversityTransportationDriverState
+    extends State<UniversityTransportationDriver> {
+  double _latitude = 0.0;
+  double _longitude = 0.0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  HubConnection _connection;
+
+  Future<void> _connectTrackingHub() async {
+    _connection = HubConnectionBuilder()
+        .withUrl(
+            ApiPath.DomainURI + '/trackingHub',
+            HttpConnectionOptions(
+              client: IOClient(
+                  HttpClient()..badCertificateCallback = (x, y, z) => true),
+              logging: (level, message) => print(message),
+            ))
+        .build();
+
+    await _connection.start();
+    _assignHubFunction();
+  }
+
+  void _assignHubFunction() {
+    if (_connection.state == HubConnectionState.connected)
+      _connection.on('ShowPointsOnMap', (message) {
+        if (message != null && message.length == 2) {
+          setState(() {
+            _latitude = message[0];
+            _longitude = message[1];
+          });
+        }
+      });
+  }
+
+  Future<void> _invokeHubSendLocation() async {
+    if (_connection.state == HubConnectionState.connected) {
+      Position currentPosition = await _determinePosition();
+      await _connection.invoke('SendGPSPoint',
+          args: [currentPosition.altitude, currentPosition.longitude]);
+    }
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _connectTrackingHub();
   }
 
   @override
@@ -45,19 +118,23 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Sever Said:',
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'latitude: $_latitude',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Text(
+              'longitude: $_longitude',
+              style: Theme.of(context).textTheme.headline6,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _invokeHubSendLocation,
+        tooltip: 'Send Location',
+        child: Icon(Icons.location_on),
       ),
     );
   }
